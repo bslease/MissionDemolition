701ccbee2a438d941cc3f422f486840de3591fc6
bslease <bslease@gmail.com>
2021-04-18 06:57:46 -0700

    add castle and camera reset

--- Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -19,11 +19,36 @@ public class FollowCam : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // if there's only one line following an if, it doesn't need braces
-        if (POI == null) return; // return if there is no POI
+        //// if there's only one line following an if, it doesn't need braces
+        //if (POI == null) return; // return if there is no POI
 
-        // Get the position of the poi
-        Vector3 destination = POI.transform.position;
+        //// Get the position of the poi
+        //Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+        // If there is no poi, return to P:[0,0,0]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the poi
+            destination = POI.transform.position;
+            // If poi is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile")
+            {
+                // if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view
+                    POI = null;
+                    // in the next update
+                    return;
+                }
+            }
+        }
+
         // Limit the X & Y to minimum values
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
---
+++ Mission Demolition Prototype/Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null) rb.Sleep();
+    }
+}

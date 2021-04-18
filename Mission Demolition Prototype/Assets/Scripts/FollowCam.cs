fd55a69b0f4a922fe175e69af95028c7326a55c8
bslease <bslease@gmail.com>
2021-04-17 18:12:57 -0700

    implement follow cam

---
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI; // The static point of interest
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
+    public float camZ; // The desired z pos of the camera
+
+    private void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        // if there's only one line following an if, it doesn't need braces
+        if (POI == null) return; // return if there is no POI
+
+        // Get the position of the poi
+        Vector3 destination = POI.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Force destination.z to be camZ to keep the camera far enough away
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
--- Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
+++ Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
@@ -80,6 +80,7 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }

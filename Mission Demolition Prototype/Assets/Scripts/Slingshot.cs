634efee80a834b0527ee9a4f18ac6bbadafa8d5f
bslease <bslease@gmail.com>
2021-04-18 14:35:09 -0700

    add a variety of castles, UI and a game loop

--- Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
+++ Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
@@ -100,6 +100,8 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 }
